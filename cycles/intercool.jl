b26d367372616f9d114d9e8bb8b9f8df3a8febb2
heathbuer <125149021+heathbuer@users.noreply.github.com>
2023-03-11 10:16:01 -0700

    Add files via upload

--- cycles/intercool.jl
+++ cycles/intercool.jl
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function w_out_net(r_p, T_1, T_3, η_c, η_t, Gas)
     κ = Gas.k
     cp = Gas.cp
@@ -19,8 +20,10 @@ end
 function η_II(T_2, T_3, r_p, Gas, T_0)
     κ = Gas.κ
     α = (κ - 1) / κ
+    T_5 = T_3;
 
 
-    return (T_2 + T_3) * (1 - r_p ^ -α) / ((T_2 + T_3) - T_0*ln(T_2 * T_3))
+    return (((1 - r_p ^ α) - ((2 * T_1 / T_5) * (rp ^ (α / 2) - 1)) / (1 - (T_1 / T_5) * rp ^ (α / 2))) / ((T_2 + T_3) - T_0*ln(T_2 * T_3))
 
 end
+=======
