5801ff59a66a25cf8bdcac81d7772028067e9ac0
jhofff2021 <127441147+jhofff2021@users.noreply.github.com>
2023-03-20 15:38:52 -0600

    Update regen.jl

--- cycles/regen.jl
+++ cycles/regen.jl
@@ -1,22 +1,23 @@
-function w_out_net(r_p, T_1, T_3, η_c, η_t, Gas)
+function w_out_net(r_p, T_min, T_max, η_c, η_t, Gas)
     κ = Gas.κ
     c_p = Gas.cp
     α = (κ - 1) / κ
     r_p_1 = sqrt(r_p)
 
-    return 42 # need to fix
+    return η_t * c_p * T_max *(1 - r_p^(-α)) - (c_p / η_c) * T_min * (r_p^(α) - 1) # need to fix
 
 end
 
-function η_th(r_p, T_1, T_3, Gas)
+function η_th(r_p, T_min, T_max, Gas)
     κ = Gas.κ
     α = (κ - 1) / κ
 
-    return 1 - ((T_1 /  T_3) * (r_p ^ α)) # Correct
+    return 1 - ((T_min /  T_max) * (r_p ^ α)) # Correct
 end
 
-function η_II(T_2, T_3, r_p, Gas, T_0)
-    κ = Gas.κ
-    α = (κ - 1) / κ
-    return (1 - T_1 /  T_3 * (r_p) ^ α) / (1 - (T_0*ln(T_2 * T_3) / (T_2 + T_3)))
-end
+
+#function η_II(T_2, T_3, r_p, Gas, T_0)
+#    κ = Gas.κ
+#    α = (κ - 1) / κ
+#    return (1 - T_1 /  T_3 * (r_p) ^ α) / (1 - (T_0*ln(T_2 * T_3) / (T_2 + T_3)))
+#end
