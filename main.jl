bcf4fbf9494c6ed3c4c2164af845a848690cce96
bennet <bennet.outland@protonmail.com>
2023-03-21 13:13:23 -0600

    minor graphing changes

--- main.jl
+++ main.jl
@@ -12,11 +12,11 @@ T_3 = 1590 #K
 η_t = 0.8
 
 # Properites to Vary
-r_p = 1:0.01:30
+r_p = 1:0.01:25
 #Gasses = [He, H, Argon, N_2, CO, CO_2, Air]
 Gasses = [N_2, Argon, Ne, He]#[Argon, N_2, CO, CO_2, Air]
 
-η_th_vs_rp(η_th, r_p, Gasses, T_1, T_3)
+η_th_vs_rp(η_th, r_p, Gasses, T_1, T_3, η_c, η_t,)
 
 #work_out_net_vs_rp(w_out_net, r_p, T_1, T_3, η_c, η_t, Gasses) # For air, r_p* = 17.2
 
