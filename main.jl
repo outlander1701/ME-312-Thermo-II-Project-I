6a3cbe8e5b4cfe04776201f50533d12ecaf4961c
bennet <bennet.outland@protonmail.com>
2023-03-24 21:13:48 -0600

    all the frickin beautiful graphs!

--- main.jl
+++ main.jl
@@ -1,11 +1,10 @@
 #include("./Exergy.jl")
-include("./cycles/simple.jl")
+#include("./cycles/simple.jl")
 #include("./cycles/intercool.jl")
 #include("./cycles/regen.jl")
-#include("./cycles/IRR.jl")
+include("./cycles/IRR.jl")
 include("./plotting.jl")
 include("./Gasses.jl")
-
 
 using Plots
 gr()
@@ -27,10 +26,10 @@ r_p = 1:0.1:60
 Gasses = [Argon, Air, He, H] #[Argon, CO_2, Air, He, H] #[Argon, N_2, CO, CO_2, Air]
 
 # Plots
-η_th_vs_rp(η_th, r_p, Gasses, T_min, T_max, η_c, η_t, ϵ, true)
+#η_th_vs_rp(η_th, r_p, Gasses, T_min, T_max, η_c, η_t, ϵ, true, true)
 
-#work_out_net_vs_rp(w_out_net, r_p, T_min, T_max, η_c, η_t, Gasses, false) # For air, r_p* = 17.2
+#work_out_net_vs_rp(w_out_net, r_p, T_min, T_max, η_c, η_t, Gasses, true) # For air, r_p* = 17.2
 
 #work_out_net_vs_η_th(η_th, w_out_net, r_p, T_min, T_max, η_c, η_t, ϵ, Gasses, true)
 
-#η_II_vs_rp(η_II, r_p, Gasses, T_min, T_max, η_c, η_t, ϵ, true)
+η_II_vs_rp(η_II, r_p, Gasses, T_min, T_max, η_c, η_t, ϵ, true, true)
