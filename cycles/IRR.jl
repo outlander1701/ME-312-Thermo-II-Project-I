cae089f02f86656f2bffaa934d689a8d7108e697
heathbuer <125149021+heathbuer@users.noreply.github.com>
2023-03-24 22:06:43 -0600

    Update IRR.jl
    Fixed eta_th

--- cycles/IRR.jl
+++ cycles/IRR.jl
@@ -27,13 +27,13 @@ function w_out_net(r_p, T_min, T_max, η_c, η_t, Gas)
     return 2*c_p*(η_t * T_max *(1 - r_p_1^(-α)) - (T_min / η_c) * (r_p_1^(α) - 1))
 end
 
-function η_th(r_p, T_min, T_max, η_c, η_t, ϵ, Gas)
+function η_th(r_p, T_min, T_max, T_5, η_c, η_t, ϵ, Gas)
     κ = Gas.κ
     c_p = Gas.cp
     α = (κ - 1) / κ
     r_p_1 = sqrt(r_p)
 
-    return w_out_net(r_p, T_min, T_max, η_c, η_t, Gas) / (c_p * (T_max * (2 - r_p_1^(-α) - ϵ*r_p_1^(-α)) + 2*T_min*r_p_1^(α)))
+    return w_out_net(r_p, T_min, T_max, η_c, η_t, Gas) / (c_p*(T_max-T_5)+(c_p*T_max*(1-r_p_1^(-α))
 end
 
 function η_II(r_p, T_min, T_max, T_0, η_c, η_t, ϵ, Gas)
