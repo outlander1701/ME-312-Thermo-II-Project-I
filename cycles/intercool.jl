2559a740e23f431b5ff38858a9f8f6f069f2a64d
bennet <bennet.outland@protonmail.com>
2023-03-24 22:27:02 -0600

    misc changes

--- cycles/intercool.jl
+++ cycles/intercool.jl
@@ -41,16 +41,6 @@ function η_th(r_p, T_1, T_3, η_c, η_t, ϵ, Gas)
 
 end
 
-"""
-function η_II(T_2, T_3, r_p, Gas, T_0)
-    κ = Gas.κ
-    α = (κ - 1) / κ
-    T_5 = T_3;
-
-    return ((1 - r_p ^ α) - ((2 * T_1 / T_5) * (r_p ^ (α / 2) - 1)) / (1 - (T_1 / T_5) * r_p ^ (α / 2))) / ((T_2 + T_3) - T_0*ln(T_2 * T_3))
-
-end
-"""
 
 function η_II(r_p, T_min, T_max, T_0, η_c, η_t, ϵ, Gas)
 
